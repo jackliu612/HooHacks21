1c9c950a134798a9d4e81c4cf9a7431a30a55271
Victor-Pham <victorp11@outlook.com>
2021-03-27 18:22:54 -0400

    Users joining

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+players = []
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+@socketio.on('userJoined')
+def add_user(data):
+    print(data)
+    players.append(data)
+    socketio.emit('addUser', data)
+
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
--- static/js/game.js
+++ static/js/game.js
@@ -1,1 +1,12 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+socket.on("connect", () => {
+    var name = prompt("Please enter your name");
+    console.log(name)
+    if(name != null){
+        socket.emit("userJoined", name);
+    }
+});
+
+
+
